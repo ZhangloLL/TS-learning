204592646bbdd6ca5aea4d93657f45bc00bb2bfe
hlzer <2085165589@qq.com>
2026-06-05 11:35:24 +0800

    async_await

---
+++ dist/02_进阶/04_async_await.js
@@ -0,0 +1,15 @@
+"use strict";
+// 并行
+function fetchUser(id) {
+    return Promise.resolve({ id, name: `User${id}` });
+}
+async function main() {
+    console.time("串行");
+    let user1 = await fetchUser(1);
+    let user2 = await fetchUser(2);
+    console.timeEnd("串行");
+    console.time("并行");
+    let results = await Promise.all([fetchUser(1), fetchUser(2)]);
+    console.timeEnd("并行");
+}
+main();
